1f750492b78eaf51769216df3610e102ff1d709a
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-11-16 15:52:24 +0900

    [midify] new function (z^2-1)*(z^2+1/4)

--- src/2Dplot.cu
+++ src/2Dplot.cu
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+//#include <cv.hpp>
 
 #include <thrust/complex.h>
 
@@ -32,12 +33,12 @@
 
 template<typename T> thrust::complex<T> vf( thrust::complex<T> z )
 {
-	return z*z*z -1.0;
+	return z*z*z*z -(3.0*z*z)/(4.0) -(1.0)/(4.0);
 }
 
 template<typename T> thrust::complex<T> df( thrust::complex<T> z )
 {
-	return 3.0*z*z;
+	return 4.0*z*z*z - (3.0*z)/(2.0);
 }
 
 template<typename T> thrust::complex<T> Newton( thrust::complex<T> z, int &count, double &er )
@@ -56,12 +57,13 @@ template<typename T> thrust::complex<T> Newton( thrust::complex<T> z, int &count
 
 template<typename T> int FixPoint( thrust::complex<T> z )
 {
-	const int nfp = 3;  // 不動点の数
+	const int nfp = 4;  // 不動点の数
 	thrust::complex<T> *fps = new thrust::complex<T> [nfp];
 
-	fps[0] = thrust::complex<T> ( 1.0, 0.0 );
-	fps[1] = thrust::complex<T> ( -0.5,  0.866025 );
-	fps[2] = thrust::complex<T> ( -0.5, -0.866025 );
+	fps[0] = thrust::complex<T> (  1.0,  0.0 );
+	fps[1] = thrust::complex<T> (  0.0,  0.5 );
+	fps[2] = thrust::complex<T> (  0.0, -0.5 );
+	fps[3] = thrust::complex<T> ( -1.0,  0.0 );
 
 	int col = 0;
 	double min = (double)(MAXIT);
@@ -98,15 +100,16 @@ void display(void)
 		for (int j=0; j<RMAX; j++)
 		{
 			int count;
-			double er, p;
+			double er;
+			//double p;
 			thrust::complex<double> z0 = thrust::complex<double>( x, y );
 			thrust::complex<double> z = Newton(z0,count,er);
 
-			p = 0.0;
-			if (count < MAXIT)
-			{
-				p = log2( -12.0 / log10(er));
-			}
+//			p = 0.0;
+//			if (count < MAXIT)
+//			{
+//				p = log2( -12.0 / log10(er));
+//			}
 
 //			double brit = (double)(1.0/MAXIT)*(MAXIT - count);
 //			double brit = p;
@@ -127,6 +130,9 @@ void display(void)
 				break;
 			case 2:
 				glColor3d(0.0,0.0,brit);
+				break;
+			case 3:
+				glColor3d(brit,0.0,brit);
 				break;
 			default:
 				glColor3d(0.0,0.0,0.0);
@@ -161,6 +167,44 @@ void resize(int w, int h)
 
 	// Screen上の表示領域をView portの大きさに比例させる
 	glOrtho( -w/ZOOM, w/ZOOM, -h/ZOOM, h/ZOOM, -1.0, 1.0);
+}
+
+void saveImage( const unsigned int imageWidth, const unsigned int imageHeight )
+{
+	const unsigned int channelNum = 3; // RGBなら3, RGBAなら4
+	void* dataBuffer = ( GLubyte* )malloc( imageWidth * imageHeight * channelNum );
+
+	// 読み取るOpneGLのバッファを指定 GL_FRONT:フロントバッファ　GL_BACK:バックバッファ
+	glReadBuffer( GL_FRONT );
+
+	// OpenGLで画面に描画されている内容をバッファに格納
+	glReadPixels(
+			0,                 //読み取る領域の左下隅のx座標
+			0,                 //読み取る領域の左下隅のy座標 //0 or getCurrentWidth() - 1
+			imageWidth,             //読み取る領域の幅
+			imageHeight,            //読み取る領域の高さ
+			GL_RGB, //it means GL_BGR,           //取得したい色情報の形式
+			GL_UNSIGNED_BYTE,  //読み取ったデータを保存する配列の型
+			dataBuffer      //ビットマップのピクセルデータ（実際にはバイト配列）へのポインタ
+	);
+	glFlush();
+
+//	GLubyte* p = static_cast<GLubyte*>( dataBuffer );
+//	std::string fname = "outputImage.jpg";
+//	IplImage* outImage = cvCreateImage( cvSize( imageWidth, imageHeight ), IPL_DEPTH_8U, 3 );
+//
+//	for ( unsigned int j = 0; j < imageHeight; ++ j )
+//	{
+//		for ( unsigned int i = 0; i < imageWidth; ++i )
+//		{
+//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 0 ] = *p;
+//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 1 ] = *( p + 1 );
+//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 2 ] = *( p + 2 );
+//			p += 3;
+//		}
+//	}
+//
+//	cvSaveImage( fname.c_str(), outImage );
 }
 
 int main(int argc, char *argv[])
