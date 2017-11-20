ed5883120d2422c185cb08fba089e1c22cf0c646
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-11-20 20:27:21 +0900

    temp

--- src/2Dplot.cu
+++ src/2Dplot.cu
@@ -14,7 +14,7 @@
 
 #include <thrust/complex.h>
 
-#define EPS 0.0000001  // 停止判定
+#define EPS 0.000001  // 停止判定
 #define MAXIT 30      // 最大反復回数
 #define ZMAX 0.8      // 初期値の最大絶対値
 #define ZOOM 600      // 拡大率
@@ -95,7 +95,7 @@ void DrawApollonius( int i, int j, double alp )
 	glColor3d(1.0,1.0,1.0);   // 白の点を描画
 	glPointSize(8.0);      // 点の大きさ（ディフォルトは1.0)
 	glBegin(GL_POINTS);
-//	glVertex2d( fps[i].real() -XOFS, fps[i].imag() );
+	glVertex2d( fps[i].real() -XOFS, fps[i].imag() );
 	glEnd();
 
 	// Apollonius円の描画
@@ -194,44 +194,6 @@ void resize(int w, int h)
 
 	// Screen上の表示領域をView portの大きさに比例させる
 	glOrtho( -(double)w/ZOOM, (double)w/ZOOM, -(double)h/ZOOM, (double)h/ZOOM, -1.0, 1.0);
-}
-
-void saveImage( const unsigned int imageWidth, const unsigned int imageHeight )
-{
-	const unsigned int channelNum = 3; // RGBなら3, RGBAなら4
-	void* dataBuffer = ( GLubyte* )malloc( imageWidth * imageHeight * channelNum );
-
-	// 読み取るOpneGLのバッファを指定 GL_FRONT:フロントバッファ　GL_BACK:バックバッファ
-	glReadBuffer( GL_FRONT );
-
-	// OpenGLで画面に描画されている内容をバッファに格納
-	glReadPixels(
-			0,                 //読み取る領域の左下隅のx座標
-			0,                 //読み取る領域の左下隅のy座標 //0 or getCurrentWidth() - 1
-			imageWidth,             //読み取る領域の幅
-			imageHeight,            //読み取る領域の高さ
-			GL_RGB, //it means GL_BGR,           //取得したい色情報の形式
-			GL_UNSIGNED_BYTE,  //読み取ったデータを保存する配列の型
-			dataBuffer      //ビットマップのピクセルデータ（実際にはバイト配列）へのポインタ
-	);
-	glFlush();
-
-//	GLubyte* p = static_cast<GLubyte*>( dataBuffer );
-//	std::string fname = "outputImage.jpg";
-//	IplImage* outImage = cvCreateImage( cvSize( imageWidth, imageHeight ), IPL_DEPTH_8U, 3 );
-//
-//	for ( unsigned int j = 0; j < imageHeight; ++ j )
-//	{
-//		for ( unsigned int i = 0; i < imageWidth; ++i )
-//		{
-//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 0 ] = *p;
-//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 1 ] = *( p + 1 );
-//			outImage->imageData[ ( imageHeight - j - 1 ) * outImage->widthStep + i * 3 + 2 ] = *( p + 2 );
-//			p += 3;
-//		}
-//	}
-//
-//	cvSaveImage( fname.c_str(), outImage );
 }
 
 int main(int argc, char *argv[])
