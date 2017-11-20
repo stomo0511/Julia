172c9e1dbbc992257d7694524d621e54be8b5c48
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-11-20 18:42:17 +0900

    [dix] Voronoi diagram

--- src/2Dplot.cu
+++ src/2Dplot.cu
@@ -44,13 +44,104 @@ void setZero( thrust::complex<double> *fps )
 	fps[4] = thrust::complex<double> ( -3.0, -3.0 );
 }
 
+void PBisec( const int i, const int j, double &a, double &b )
+{
+	const double a1 = fps[i].real();
+	const double a2 = fps[j].real();
+	const double b1 = fps[i].imag();
+	const double b2 = fps[j].imag();
+
+//	std::cout << "a1 = " << a1 << ", b1 = " << b1 << ", a2 = " << a2 << ", b2 = " << b2 << std::endl;
+
+	a = (b2 - b1) / (a2 - a1);
+	b = (a2*b1 - a1*b2) / (a2 - a1);
+
+//	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	a = -1.0 / a;  // 傾き = 2点を通る直線に垂直
+	// 2点の中点座標
+	double mx = (a1+a2)/2.0;
+	double my = (b1+b2)/2.0;
+//	std::cout << "mx = " << mx << ", my = " << my << std::endl;
+
+	b = my - a*mx;
+}
+
+// 領域0 枠（黒）
 void Reg0()
 {
-	gl
+	glColor3d(0.0,0.0,0.0);
 	glBegin(GL_POLYGON);
+	glVertex2d( 4.0, 4.0);
+	glVertex2d( 4.0,-4.0);
+	glVertex2d(-4.0,-4.0);
+	glVertex2d(-4.0, 4.0);
 	glEnd();
 }
 
+//零点 0 の Voronoi Cell
+void Cell0()
+{
+	glColor3d(1.0,0.0,0.0);
+	glBegin(GL_POLYGON);
+	glVertex2d( 0.0, 2.0 );
+	glVertex2d(  2.3571, -0.3571 );
+	glVertex2d( 0.0, -2.125 );
+	glVertex2d( -2.3571, -0.3571 );
+	glEnd();
+}
+
+// 零点 1 の Voronoi Cell
+void Cell1()
+{
+	glColor3d(0.0,1.0,0.0);
+	glBegin(GL_POLYGON);
+	glVertex2d( 0.0, 2.0 );
+	glVertex2d( 2.3571, -0.3571 );
+	glVertex2d( 4.0, 0.3 );
+	glVertex2d( 4.0, 4.0 );
+	glVertex2d( 0.0, 4.0 );
+	glEnd();
+}
+
+// 零点 2 の Voronoi Cell
+void Cell2()
+{
+	glColor3d(0.0,0.0,1.0);
+	glBegin(GL_POLYGON);
+	glVertex2d( 0.0, 2.0 );
+	glVertex2d( -2.3571, -0.3571 );
+	glVertex2d( -4.0, 0.3 );
+	glVertex2d( -4.0, 4.0 );
+	glVertex2d( 0.0, 4.0 );
+	glEnd();
+}
+
+// 零点 3 の Voronoi Cell
+void Cell3()
+{
+	glColor3d(1.0,0.0,1.0);
+	glBegin(GL_POLYGON);
+	glVertex2d( 2.3571, -0.3571 );
+	glVertex2d( 0.0, -2.125 );
+	glVertex2d( 0.0, -4.0 );
+	glVertex2d( 4.0, -4.0 );
+	glVertex2d( 4.0,  0.3 );
+	glEnd();
+}
+
+// 零点 4 の Voronoi Cell
+void Cell4()
+{
+	glColor3d(0.0,1.0,1.0);
+	glBegin(GL_POLYGON);
+	glVertex2d( -2.3571, -0.3571 );
+	glVertex2d( 0.0, -2.125 );
+	glVertex2d( 0.0, -4.0 );
+	glVertex2d( -4.0, -4.0 );
+	glVertex2d( -4.0,  0.3 );
+	glEnd();
+}
 void display(void)
 {
 	//////////////////////////////////////////////
@@ -58,10 +149,23 @@ void display(void)
 	glClearColor(1.0, 1.0, 1.0, 1.0); // 塗りつぶしの色を指定
 	glClear(GL_COLOR_BUFFER_BIT);     // 塗りつぶし
 
-	setZero(fps);     // 零点のセット
-
 	// 領域 0 の描画（ \pm4 + \pm 4 i）の枠
 	Reg0();
+
+	//零点 0 の Voronoi Cell
+	Cell0();
+
+	//零点 1 の Voronoi Cell
+	Cell1();
+
+	//零点 2 の Voronoi Cell
+	Cell2();
+
+	//零点 3 の Voronoi Cell
+	Cell3();
+
+	//零点 4 の Voronoi Cell
+	Cell4();
 
 	glFlush();
 	//////////////////////////////////////////////
@@ -81,6 +185,28 @@ void resize(int w, int h)
 
 int main(int argc, char *argv[])
 {
+	setZero(fps);     // 零点のセット
+
+	// 垂直二等分線
+	double a, b;
+	PBisec( 0, 1, a, b );
+	std::cout << "(0,1) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
+	PBisec( 0, 2, a, b );
+	std::cout << "(0,2) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
+	PBisec( 0, 3, a, b );
+	std::cout << "(0,3) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
+	PBisec( 0, 4, a, b );
+	std::cout << "(0,4) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
+	PBisec( 1, 3, a, b );
+	std::cout << "(1,3) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
+	PBisec( 2, 4, a, b );
+	std::cout << "(2,4) の垂直二等分線の傾き : " << a << ", 切片 : " << b << std::endl;
+
 	glutInit(&argc, argv);          // OpenGL初期化
 	glutInitWindowSize(1100,1100);  // 初期Windowサイズ指定
 	glutCreateWindow(argv[0]);      // Windowを開く
