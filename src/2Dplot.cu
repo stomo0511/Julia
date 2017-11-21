e1e454e7598bd72cb79e5e7ab79154fd75100422
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-11-21 20:22:48 +0900

    temp

--- src/2Dplot.cu
+++ src/2Dplot.cu
@@ -12,20 +12,16 @@
 #include <cstdlib>
 #include <cmath>
 #include <cassert>
-
+#include <GLUT/glut.h>
 #include <thrust/complex.h>
+#include <thrust/host_vector.h>
 
 #define EPS 0.000001  // 停止判定
 #define MAXIT 30      // 最大反復回数
 #define ZMAX 4.0      // 初期値の最大絶対値
 #define ZOOM 200      // 拡大率
 #define RMAX 2000     // 複素平面の分割数
-#define ORD  2        // 次数
-
-#include <GLUT/glut.h>
-//#ifndef glutCloseFunc
-//#define glutCloseFunc glutWMCloseFunc
-//#endif
+#define ORD  32        // Nourein法の次数
 
 #define NFP 5 // 零点の数
 thrust::complex<double> fps[NFP];
@@ -40,27 +36,27 @@ void setZero( thrust::complex<double> *fps )
 }
 
 // Polynomial
-template<typename T> thrust::complex<T> vf( thrust::complex<T> z )
+thrust::complex<double> vf( thrust::complex<double> z )
 {
-	thrust::complex<T> iu = thrust::complex<T> ( 0.0, 1.0 );
+	thrust::complex<double> iu = thrust::complex<double> ( 0.0, 1.0 );
 	return z*z*z*z*z + iu*z*z*z*z + + 3.0*z*z*z + 41.0*iu*z*z + 132.0*z -90.0*iu;
 }
 
 // derived function of the polynomial
-template<typename T> thrust::complex<T> df( thrust::complex<T> z )
+thrust::complex<double> df( thrust::complex<double> z )
 {
-	thrust::complex<T> iu = thrust::complex<T> ( 0.0, 1.0 );
+	thrust::complex<double> iu = thrust::complex<double> ( 0.0, 1.0 );
 	return 5.0*z*z*z*z + 4.0*iu*z*z*z + 9.0*z*z + 82.0*iu*z + 132.0;
 }
 
 // Nourein subfunction
-template<typename T> thrust::complex<T> vc( const int K, thrust::complex<T> z )
+thrust::complex<double> vc( const int K, thrust::complex<double> z )
 {
-	thrust::complex<T> f = thrust::complex<T> (0.0,0.0);;
+	thrust::complex<double> f = thrust::complex<double> (0.0,0.0);;
 
 	for (int i=0; i<NFP; i++)
 	{
-		thrust::complex<T> tmp = thrust::complex<T> (1.0,0.0);
+		thrust::complex<double> tmp = thrust::complex<double> (1.0,0.0);
 
 		// tmp = (z_i -z)^{k+1}
 		for (int k=0; k<=K; k++)
@@ -75,7 +71,7 @@ template<typename T> thrust::complex<T> vc( const int K, thrust::complex<T> z )
 	return f;
 }
 
-template<typename T> thrust::complex<T> Nourein( const int p, thrust::complex<T> z, int &count, double &er )
+thrust::complex<double> Nourein( const int p, thrust::complex<double> z, int &count, double &er )
 {
 	assert(p>=2);
 
@@ -91,10 +87,37 @@ template<typename T> thrust::complex<T> Nourein( const int p, thrust::complex<T>
 	return z;
 }
 
-template<typename T> int FixPoint( thrust::complex<T> z )
+void DrawApollonius( int i, int j, double alp )
+{
+	const int pts = 180;    // 円周上の点数
+
+	thrust::complex<double> center = (fps[i] - alp*alp*fps[j]) / (1.0 - alp*alp);
+	double radius = alp*abs(fps[i] - fps[j]) / (1.0 - alp*alp);
+	double tic = (double)(2.0*M_PI / pts);
+
+	// Z_i の描画
+	glColor3d(1.0,1.0,1.0);   // 白の点を描画
+	glPointSize(8.0);      // 点の大きさ（ディフォルトは1.0)
+	glBegin(GL_POINTS);
+	glVertex2d( fps[i].real(), fps[i].imag() );
+	glEnd();
+
+	// Apollonius円の描画
+	glColor3d(1.0,1.0,1.0);   // 白の円を描画
+	glLineWidth(2.0);         // 線の太さ（ディフォルトは1.0）
+
+	glBegin(GL_LINE_LOOP);
+	for (int i=1; i<pts; i++)
+	{
+		glVertex2d( center.real() + radius*cos( tic*i )  , center.imag() + radius*sin( tic*i ) );
+	}
+	glEnd();
+	glFlush();
+}
+
+int FixPoint( thrust::complex<double> z )
 {
 	int col = 0;
-//	int col = 1;
 	double min = (double)MAXIT;
 
 	for (int i=0; i<NFP; i++)
@@ -116,8 +139,6 @@ void display(void)
 	glClearColor(1.0, 1.0, 1.0, 1.0); // 塗りつぶしの色を指定
 	glClear(GL_COLOR_BUFFER_BIT);     // 塗りつぶし
 
-	setZero(fps);     // 零点のセット
-
 	//////////////////////////////////////////////
 	// 点の描画
 	glBegin(GL_POINTS);
@@ -133,30 +154,34 @@ void display(void)
 			thrust::complex<double> z0 = thrust::complex<double>( x, y );
 			thrust::complex<double> z = Nourein(ORD,z0,count,er);
 
-			double brit;
-			if (count > 13)
-				brit = 0.0;
+			int grad = 16;  // 明るさの階調
+			double bright;
+			if (count > grad)
+				bright = 0.0;
 			else
-				brit = (13.0 - double(count)) / 13.0;
+			{
+				// 反復回数1回が最も明るく（bright=1）となるように修正（count-1）
+				bright = double(grad - (count-1)) / double(grad);
+			}
 			// 明るさの補正
-			brit += 0.2;
+//			bright += 0.2;
 
 			switch( FixPoint(z) )  // 塗りつぶし色の設定
 			{
 			case 0:
-				glColor3d(brit,0.0,0.0);
+				glColor3d(bright,0.0,0.0);
 				break;
 			case 1:
-				glColor3d(0.0,brit,0.0);
+				glColor3d(0.0,bright,0.0);
 				break;
 			case 2:
-				glColor3d(0.0,0.0,brit);
+				glColor3d(0.0,0.0,bright);
 				break;
 			case 3:
-				glColor3d(brit,0.0,brit);
+				glColor3d(bright,0.0,bright);
 				break;
 			case 4:
-				glColor3d(0.0,brit,brit);
+				glColor3d(0.0,bright,bright);
 				break;
 			default:
 				glColor3d(0.0,0.0,0.0);
@@ -169,8 +194,10 @@ void display(void)
 		x += (double)(2*ZMAX / RMAX);
 	}
 	glEnd();
+	//////////////////////////////////////////////
 
-	// Z_i の描画
+	//////////////////////////////////////////////
+	// 零点の描画
 	for (int i=0; i<NFP; i++)
 	{
 		glColor3d(1.0,1.0,1.0);   // 白の点を描画
@@ -179,9 +206,10 @@ void display(void)
 		glVertex2d( fps[i].real(), fps[i].imag() );
 		glEnd();
 	}
+	//////////////////////////////////////////////
 
 	glFlush();
-	//////////////////////////////////////////////
+
 }
 
 void resize(int w, int h)
@@ -198,6 +226,8 @@ void resize(int w, int h)
 
 int main(int argc, char *argv[])
 {
+	setZero(fps);     // 零点のセット
+
 	glutInit(&argc, argv);          // OpenGL初期化
 	glutInitWindowSize(1100,1100);  // 初期Windowサイズ指定
 	glutCreateWindow(argv[0]);      // Windowを開く
