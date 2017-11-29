efaa5d85d03d3fe0688d9b572592f4c36f2553e7
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-11-29 11:35:10 +0900

    [fix] vectorize

--- src/2Dplot.cu
+++ src/2Dplot.cu
@@ -11,71 +11,81 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <GLUT/glut.h>
 #include <thrust/complex.h>
 
-#define EPS 0.0000001  // 停止判定
-#define MAXIT 40      // 最大反復回数
-#define ZMAX 1.5      // 初期値の最大絶対値
-#define ZOOM 500      // 拡大率
-#define RMAX 2000     // 複素平面の分割数
+#define EPS 0.000001  // 停止判定
+#define MAXIT 30      // 最大反復回数
+#define ZMAX 2.0      // 初期値の最大絶対値
+#define ZOOM 300      // 拡大率
+#define RMAX 1000     // 複素平面の分割数
 
-#if defined (__APPLE__) || defined(MACOSX)
-  #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-  #include <GLUT/glut.h>
-  #ifndef glutCloseFunc
-  #define glutCloseFunc glutWMCloseFunc
-  #endif
-#else
-#include <GL/freeglut.h>
-#endif
+// Zeros
+std::vector< thrust::complex<double> > Zrs {
+	thrust::complex<double> (  1.0,  0.0 ),
+	thrust::complex<double> ( -0.5,  0.866025 ),
+	thrust::complex<double> ( -0.5, -0.866025 )
+};
 
-template<typename T> thrust::complex<T> vf( thrust::complex<T> z )
+// Coefficients
+std::vector< thrust::complex<double> > Cef {
+	thrust::complex<double> (  1.0, 0.0 ),  // Z^3
+	thrust::complex<double> (  0.0, 0.0 ),  // z^2
+	thrust::complex<double> (  0.0, 0.0 ),  // z^1
+	thrust::complex<double> ( -1.0, 0.0 )   // z^0
+};
+
+// Hornet method for polynomial
+template<typename T> void Horner( std::vector< thrust::complex<T> > cf, thrust::complex<T> z,
+					thrust::complex<T> &vf, thrust::complex<T> &df )
 {
-	return z*z*z -1.0;
-}
+	vf = Cef[0];
+	df = thrust::complex<T> (0.0,0.0);
+	thrust::complex<T> tmp;
 
-template<typename T> thrust::complex<T> df( thrust::complex<T> z )
-{
-	return 3.0*z*z;
+    for(auto itr = Cef.begin()+1; itr < Cef.end(); ++itr)
+    {
+    	tmp = vf;
+    	vf = vf*z + *itr;
+    	df = df*z + tmp;
+    }
 }
 
 template<typename T> thrust::complex<T> Newton( thrust::complex<T> z, int &count, double &er )
 {
+	thrust::complex<T> vf, df;
+	Horner( Cef, z, vf, df );
 	count = 0;
 
-	while ((count < MAXIT) && (abs(vf(z)) > EPS))
+	while ((count < MAXIT) && (abs(vf) > EPS))
 	{
-		z -= vf(z) / df(z);
+		z -= vf / df;
+		Horner( Cef, z, vf, df );
 		count++;
 	}
-	er = abs(vf(z));
+	er = abs(vf);
 
 	return z;
 }
 
 template<typename T> int FixPoint( thrust::complex<T> z )
 {
-	const int nfp = 3;  // 不動点の数
-	thrust::complex<T> *fps = new thrust::complex<T> [nfp];
-
-	fps[0] = thrust::complex<T> ( 1.0, 0.0 );
-	fps[1] = thrust::complex<T> ( -0.5,  0.866025 );
-	fps[2] = thrust::complex<T> ( -0.5, -0.866025 );
-
+	int i = 0;
 	int col = 0;
 	double min = (double)(MAXIT);
 
-	for (int i=0; i<nfp; i++)
+	for (auto itr = Zrs.begin(); itr < Zrs.end(); ++itr )
 	{
-		if (abs(z - fps[i]) < min)
+		if (abs( z - *itr) < min)
 		{
-			min = abs(z - fps[i]);
+			min = abs( z - *itr);
 			col = i;
 		}
+		i++;
 	}
-	delete[] fps;
-
 	return col;
 }
 
@@ -98,37 +108,30 @@ void display(void)
 		for (int j=0; j<RMAX; j++)
 		{
 			int count;
-			double er, p;
+			double er;
 			thrust::complex<double> z0 = thrust::complex<double>( x, y );
 			thrust::complex<double> z = Newton(z0,count,er);
 
-			//			double brit = (double)(1.0/MAXIT)*(MAXIT - count);
-
-//			p = 0.0;
-//			if (count < MAXIT)
-//			{
-//				p = log2( -12.0 / log10(er));
-//			}
-//			double brit = p;
-
-			double brit;
-			if (count > 13)
-				brit = 0.0;
+			int grad = 16;  // 明るさの階調
+			double bright;
+			if (count > grad)
+				bright = 0.0;
 			else
-				brit = (13.0 - double(count)) / 13.0;
-			// 明るさの補正
-			brit += 0.1;
+			{
+				// 反復回数1回が最も明るく（bright=1）となるように修正（count-1）
+				bright = double(grad - (count-1)) / double(grad);
+			}
 
 			switch( FixPoint(z) )  // 塗りつぶし色の設定
 			{
 			case 0:
-				glColor3d(brit,0.0,0.0);
+				glColor3d(bright,0.0,0.0);
 				break;
 			case 1:
-				glColor3d(0.0,brit,0.0);
+				glColor3d(0.0,bright,0.0);
 				break;
 			case 2:
-				glColor3d(0.0,0.0,brit);
+				glColor3d(0.0,0.0,bright);
 				break;
 			default:
 				glColor3d(0.0,0.0,0.0);
@@ -136,19 +139,24 @@ void display(void)
 			}
 
 			glVertex2d( z0.real(), z0.imag() );  // 点の描画
-			//if (count >= MAXIT-4)
-//			if (count == 4)
-//			{
-//				std::cout << "z0 = " << z0 << ", count = " << count;
-//				std::cout << ", z = " << z << ", bright = " << brit;
-//				std::cout << ", color = " << FixPoint(z) << ", p = " << p;
-//				std::cout << ", er = " << er << std::endl;
-//			}
 			y += (double)(2*ZMAX / RMAX);
 		}
 		x += (double)(2*ZMAX / RMAX);
 	}
 	glEnd();
+
+	//////////////////////////////////////////////
+	// 零点の描画
+	for (auto itr = Zrs.begin(); itr < Zrs.end(); ++itr)
+	{
+		glColor3d(1.0,1.0,1.0);   // 白の点を描画
+		glPointSize(8.0);      // 点の大きさ（ディフォルトは1.0)
+		glBegin(GL_POINTS);
+		glVertex2d( (*itr).real(), (*itr).imag() );
+		glEnd();
+	}
+	//////////////////////////////////////////////
+
 	glFlush();
 	//////////////////////////////////////////////
 }
